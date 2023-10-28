ba4f8471d4055ea4cec5db1db35e86373cda34ea
masakihumphrey588 <146698531+masakihumphrey588@users.noreply.github.com>
2023-10-28 21:27:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['254796926651', 'me', true],
+  ['254796926651', 'Assassin', true], 
   [''] 
 ] //Number of owners
 
